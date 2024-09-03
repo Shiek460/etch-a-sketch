887e8bd5cd991043994d2afee20a0a839ef0652c
Logan Lickfield <l.lickfield@gmail.com>
2024-09-03 12:47:04 -0400

    Starting point Created. Created conainer div in HTML, have a stylesheet, a js sheet with steps needed to be achieved.

---
+++ etch.js
@@ -0,0 +1,15 @@
+//Create a 16X16 grid of square divs
+
+//Button that sends a popup asking for # squares per side of grid
+    //Popup, ask
+    //Limit input number to 100
+    //Remove existing grid, populate SAME total space with new grid
+
+//Create the divs with JS
+
+//Style the divs to appear as a grid
+
+//Append the divs to ".grid-container" (in index.html)
+
+//Set up a "hover" effect to change div color when mouse passes over them
+
