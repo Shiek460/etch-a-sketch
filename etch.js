41ebf628d9a13a7aea262a4051234191c2a43adb
Logan Lickfield <l.lickfield@gmail.com>
2024-09-06 14:57:56 -0400

    Mouseover event changes rgb values properly. I believe I was inadvertently overriding the element's entire "style" setting with setAttribute('style', 'background-color: x') instead of adding only the background-color change.

--- etch.js
+++ etch.js
@@ -14,11 +14,13 @@ btn.addEventListener("click", () => {
         //switch case - is number or is greater than 100 **ADD THIS**
         if (num > 100) {
             verifyGridSize(num = prompt("Too many squares. Please choose a number up to 100 to prevent crashing."));
+        } else if (isNaN(parseInt(num))) {
+            verifyGridSize(num = prompt("Not a number. Please use a number value of 100 or less."))
         }
     }
     verifyGridSize(numOfSquares = prompt("How many squares do you want in your etch-a-sketch?"));
-    border.style.display = "none";
-    btn.textContent = "Refresh";
+    border.style.display = "flex";
+    btn.textContent = "Shake it to reset!";
     squareWidth = (1/numOfSquares)*100 + "%";
     let totalSquares = numOfSquares*numOfSquares;
     //Remove existing grid, populate SAME total space with new grid
@@ -31,10 +33,9 @@ btn.addEventListener("click", () => {
         }
     }
     //Style the divs to appear as a grid
-    function createPixel(element, num) {
+    function createPixel(element) {
         element.classList.toggle('pixel');
         element.setAttribute('style', `width: ${squareWidth}; height: ${squareWidth}; background-color: red;`);
-        //Set up a "hover" effect to change div color when mouse passes over them
         return element;
     }
     //Append the divs to ".grid-container" (in index.html)
@@ -42,11 +43,21 @@ btn.addEventListener("click", () => {
         deleteGrid();
         for (let i = 0; i < num; i++) {
             let square = document.createElement('div');
-            createPixel(square, i);
+            createPixel(square);
             container.appendChild(square)
         }
     }
     //Create the divs with JS
     populateGrid(totalSquares);
+    let colors = document.querySelectorAll('.pixel')
+        colors.forEach((color) => {
+            let r = (Math.random()*255).toFixed();
+            let g = (Math.random()*255).toFixed();
+            let b = (Math.random()*255).toFixed();
+            color.addEventListener('mouseenter', () => {
+                color.style.backgroundColor = `rgb(${r} ${g} ${b})`;
+            })
+        })
 })
 
+//Set up a "hover" effect to change div color when mouse passes over them
